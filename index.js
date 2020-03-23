5e781ba3c4c73808ce43cd656c9957695e77d075
Austin Bliss <abliss2@live.com>
2020-03-23 01:32:49 -0400

    in progress test shenanagins

---
+++ index.js
@@ -0,0 +1,53 @@
+const validatePassword = (pass) => {
+
+    const isLong = checkLength(pass)
+    const isLower = checkLower(pass)
+    const isUpper = checkCaps(pass)
+    const isNum = checkNum(pass)
+    const isSpecial = true //checkSpecial(pass)
+
+    if (isLong && isLower && isUpper && isNum && isSpecial) return true
+
+    return false
+}
+
+const checkLength = (pass) => pass.length >= 8 ? true : false
+
+const checkLower = (pass) => {
+    for (let i = 0; i < pass.length; i++) {
+        if (pass.charCodeAt(i) >= 97 && pass.charCodeAt(i) <= 122) return true
+
+    }
+    return false
+}
+
+const checkCaps = (pass) => {
+    for (let i = 0; i < pass.length; i++) {
+        if (pass.charCodeAt(i) >= 65 && pass.charCodeAt(i) <= 90) return true
+
+    }
+    return false
+}
+
+const checkNum = (pass) => {
+    for (let i = 0; i < pass.length; i++) {
+        if (pass.charCodeAt(i) >= 48 && pass.charCodeAt(i) <= 57) return true
+
+        return false
+    }
+}
+
+const checkSpecial = (pass) => {
+    for (let i = 0; i < pass.length; i++) {
+        console.log(pass[i])
+        if (pass[i] === '!' /*pass.charCodeAt(i) >= 33 && pass.charCodeAt(i) <= 47
+            /*|| pass.charCodeAt(i) >= 58 && pass.charCodeAt(i) <= 64
+            || pass.charCodeAt(i) >= 91 && pass.charCodeAt(i) <= 96
+            || pass.charCodeAt(i) >= 123 && pass.charCodeAt(i) <= 126*/) return true
+
+    }
+    return false
+}
+
+
+module.exports = validatePassword
